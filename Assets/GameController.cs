8d5d0d957fdbcd049a978745e1962b4cc83c2acf
Dor Grinshpan <dorgrin6@gmail.com>
2017-10-19 19:20:46 +0300

    assets

---
+++ Assets/GameController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+    [SerializeField] GameObject playerShip;
+    Game currentGame;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/OculusToCardboard/FauxInputTracking.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.VR;
+
+public class FauxInputTracking : MonoBehaviour {
+    private static FauxInputTracking instance = null;
+
+	// Use this for initialization
+	void Awake () {
+	    if (instance != null) {
+	        Debug.LogError("Multiple FauxInputTracking instances.");
+	    }
+	    instance = this;
+	}
+
+    public static Quaternion GetLocalRotation(VRNode node) {
+        if (node != VRNode.Head) {
+            Debug.LogError("GetLocalRotation for other than head");
+        }
+        return instance.transform.localRotation;
+    }
+
+    public static void Recenter() {
+        instance.transform.localRotation = Quaternion.identity;
+    }
+	
+}
+
